0f0f6f440925eb31e987a5b400820cefa88b108b
Gabriel <gabriel113@web.de>
2023-02-24 12:16:26 +0100

    ~Spring 2.0 ~ Second attend to create an Spring Program Order Management System
    ~State of the Art: Trying to insert the OrderManagementSystem to the Fancy Writer F**k S++T to simply run the APP with the Spring ID style.... (Not working)

--- src/main/java/com/programgripsafes/ordersystem/OrderManagementSystem/HelloWOrld.java
+++ src/main/java/com/programgripsafes/ordersystem/OrderManagementSystem/HelloWOrld.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+/*
 
 @RestController
 public class HelloWOrld {
@@ -22,9 +23,12 @@ public class HelloWOrld {
     this.text = text;
 
     }
-*/
+
+ */
+/*
     @RequestMapping ("/")
     public String index() {
         return normalText.WriteText("hi Gab!");
     }
 }
+*/
